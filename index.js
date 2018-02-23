4ff91e5f9d49d83dee9fb720a717a67e310e42ac
mrspinn <34923439+mrspinn@users.noreply.github.com>
2018-02-23 14:26:43 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -17,19 +17,23 @@ var storageRef = storage.ref();
 window.addEventListener("DOMContentLoaded", onDeviceReady, false);
 
 
-function onDeviceReady () {
+function onDeviceReady() {
   document.getElementById('btnLogin').addEventListener('click', loginUser, false);
-  document.getElementById('btnSignUp').addEventListener('click', createAccount, false);
+  document.getElementById('btnReg').addEventListener('click', createAccount, false);
   document.getElementById('btnLogout').addEventListener('click', signOut, false);
   document.getElementById('btnSaveProfile').addEventListener('click', saveProfile, false);
+  document.getElementById('btnEditProfile').addEventListener('click', editProfile, false);
+  document.getElementById('btnBackToMessage').addEventListener('click', backToMessage, false);
   auth.onAuthStateChanged(function(user) {
     if (user) {
       currentUser = user;
-      userDiv.innerHTML = user.email;
+      showHide('currentUserDiv', 'login');
+      document.getElementById('currentUser').innerHTML = user.email;
     } else {
-      userDiv.innerHTML = 'No user is signed in';
+      showHide('login', 'currentUserDiv');
     }
   });
+  getOnlineUsers();
 }
 
 function createAccount() {
@@ -41,7 +45,24 @@ function createAccount() {
     var errorMessage = error.message;
     alert("Error Code: " + errorCode + "\nError Message: " + errorMessage);
   }).then(function() {
-    console.log("Login Successful");
+    setTimeout(function() {
+      var userObj = {
+        uid: currentUser.uid,
+        email: email,
+        type: "user",
+        online: true,
+        userName: "Anonymous"
+      };
+      base.ref('users').child(currentUser.uid).set(userObj).then(function(error) {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log("Account created");
+          getOnlineUsers();
+          alert("Account Created", '', 'Success');
+        }
+      });
+    }, 10);
   });
 }
 
@@ -52,6 +73,16 @@ function loginUser() {
     .then(function() {
       console.log("Login Successful");
       currentUser = auth.currentUser;
+      base.ref('users').child(currentUser.uid).update({
+        online: true
+      }).then(function(error) {
+        if (error) {
+          console.error(error);
+        } else {
+          getOnlineUsers();
+          console.log("User now online");
+        }
+      });
     }, function(error) {
       if (error.code === 'auth/wrong-password') {
         console.log("Wrong PassWord");
@@ -63,30 +94,88 @@ function loginUser() {
 }
 
 function signOut() {
+  base.ref('users').child(currentUser.uid).update({
+    online: false
+  }).then(function(error) {
+    getOnlineUsers();
+    if (error) {
+      console.error(error);
+    }
+  });
   auth.signOut().then(function() {
     console.log("Signed Out");
   }).catch(function(error) {
     console.log(error);
+    base.ref('users').child(currentUser.uid).update({
+      online: true
+    }).then(function(error) {
+      if (error) {
+        console.error(error);
+      } else {
+        // setTimeout(function(){ getOnlineUsers(); }, 1000);
+        getOnlineUsers();
+        console.log("User now offline");
+      }
+    });
   });
 }
 
+function editProfile() {
+  showHide('profile', 'messaging');
+  showHide('btnBackToMessage', 'btnEditProfile');
+  firebase.database().ref('/users/' + currentUser.uid).once('value').then(
+    function(snapshot) {
+      document.getElementById('firstName').value = snapshot.val().firstName;
+      document.getElementById('lastName').value = snapshot.val().lastName;
+      document.getElementById('userName').value = snapshot.val().userName;
+    }
+  );
+}
+
 function saveProfile() {
-
   var user = auth.currentUser;
-
-  var userObj = {
+  var userID = user.uid;
+  base.ref('users').child(userID).update({
     firstName: document.querySelector('#firstName').value,
     lastName: document.querySelector('#lastName').value,
     userName: document.querySelector('#userName').value
-  };
-  var userID = user.uid;
-
-  base.ref('users').child(userID).set(userObj).then(function(error) {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log("Profile Updated");
-      alert("Profile updated", '', 'Success');
+  }).then(
+    function(error) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("Profile Updated");
+        alert("Profile updated", '', 'Success');
+        showHide('messaging', 'profile');
+      }
     }
+  );
+}
+
+function showHide(toShow, toHide) {
+  document.getElementById(toShow).classList.add('show');
+  document.getElementById(toHide).classList.add('hide');
+  document.getElementById(toShow).classList.remove('hide');
+  document.getElementById(toHide).classList.remove('show');
+}
+
+function backToMessage() {
+  showHide('btnEditProfile', 'btnBackToMessage');
+  showHide('messaging', 'profile');
+}
+
+function getOnlineUsers() {
+  var list = document.getElementById("onlineUsers");
+  list.innerHTML = "";
+  var users = base.ref("users");
+  users.on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var user = childSnapshot.val();
+      if (user.online && (list.innerHTML.search(user.userName) == -1)) {
+        // list.innerHTML += "<li id='" + user.nodeValue + "'>" + user.userName + "</li>";
+        list.innerHTML += "<li>" + user.userName + "</li>";
+        console.log(user.userName);
+      }
+    });
   });
 }
