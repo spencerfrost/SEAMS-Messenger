535baefdc5b8a30aaa1fc0bea7e2e8fa03114481
evanrobertmiller <36711474+evanrobertmiller@users.noreply.github.com>
2018-03-19 19:02:03 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -13,23 +13,63 @@ var base = firebase.database();
 var auth = firebase.auth();
 var storage = firebase.storage();
 var storageRef = storage.ref();
+var msgCount;
+var convos = [];
 
 window.addEventListener("DOMContentLoaded", onDeviceReady, false);
+window.onbeforeunload = function(event) {
+  // do stuff here
+  signOut();
+};
 
 
-function onDeviceReady () {
+function onDeviceReady() {
   document.getElementById('btnLogin').addEventListener('click', loginUser, false);
-  document.getElementById('btnSignUp').addEventListener('click', createAccount, false);
+  document.getElementById('btnReg').addEventListener('click', createAccount, false);
   document.getElementById('btnLogout').addEventListener('click', signOut, false);
   document.getElementById('btnSaveProfile').addEventListener('click', saveProfile, false);
-  auth.onAuthStateChanged(function(user) {
-    if (user) {
-      currentUser = user;
-      userDiv.innerHTML = user.email;
+  document.getElementById('btnEditProfile').addEventListener('click', editProfile, false);
+  document.getElementById('btnBackToMessage').addEventListener('click', backToMessage, false);
+  document.getElementById('btnMsgSend').addEventListener('click', sendMessage, false);
+  document.getElementById('btnEmoji').addEventListener('click', addEmoji, false);
+  document.getElementById('msgInput').addEventListener("keyup", function(event) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      sendMessage();
+    }
+    if (this.value != '' && auth.currentUser) {
+      enable('#btnMsgSend');
     } else {
-      userDiv.innerHTML = 'No user is signed in';
+      disable('#btnMsgSend');
     }
   });
+  auth.onAuthStateChanged(function(user) {
+    if (user) {
+      currentUser = auth.currentUser;
+      showElement('currentUserDiv');
+      hideElement('login');
+      showElement('onlineUsersDiv');
+      document.getElementById('currentUser').innerHTML = user.email;
+
+      firebase.database().ref('/users/' + currentUser.uid).once('value').then(
+        function(snapshot) {
+          if (snapshot.val().type == "admin") {
+            showElement("adminPanel");
+          }
+        }
+      );
+
+    } else {
+      showElement('login');
+      hideElement('currentUserDiv');
+      hideElement('onlineUsersDiv');
+      hideElement('adminPanel');
+    }
+  });
+  getOnlineUsers();
+  getMessages("Main");
 }
 
 function createAccount() {
@@ -41,7 +81,24 @@ function createAccount() {
     var errorMessage = error.message;
     alert("Error Code: " + errorCode + "\nError Message: " + errorMessage);
   }).then(function() {
-    console.log("Login Successful");
+    setTimeout(function() {
+      var userObj = {
+        uid: currentUser.uid,
+        email: email,
+        type: "user",
+        online: true,
+        userName: "Anonymous"
+      };
+      base.ref('users').child(currentUser.uid).set(userObj).then(function(error) {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log("Account created");
+          getOnlineUsers();
+          alert("Account Created", '', 'Success');
+        }
+      });
+    }, 10);
   });
 }
 
@@ -50,43 +107,372 @@ function loginUser() {
   var password = document.querySelector('#txtPassword').value;
   auth.signInWithEmailAndPassword(email, password)
     .then(function() {
-      console.log("Login Successful");
-      currentUser = auth.currentUser;
-    }, function(error) {
-      if (error.code === 'auth/wrong-password') {
-        console.log("Wrong PassWord");
-      }
-      if (error.code === 'auth/user-not-found') {
-        console.log("User Not Found");
-      }
-    });
+        console.log("Login Successful");
+        currentUser = auth.currentUser;
+        base.ref('users').child(currentUser.uid).update({
+          online: true
+        }).then(function(err) {
+          if (err) {
+            console.error(err);
+          } else {
+            getOnlineUsers();
+            console.log("User now online");
+            if (auth.currentUser) {
+              document.querySelector('#msgContainer').innerHTML = '';
+              getMessages("Main");
+              getConvos();
+            }
+          }
+        });
+      },
+      function(error) {
+        if (error.code === 'auth/wrong-password') {
+          console.log("Wrong PassWord");
+        }
+        if (error.code === 'auth/user-not-found') {
+          console.log("User Not Found");
+        }
+      });
 }
 
 function signOut() {
+  base.ref('users').child(currentUser.uid).update({
+    online: false
+  }).then(function(error) {
+    getOnlineUsers();
+    if (error) {
+      console.error(error);
+    }
+  });
   auth.signOut().then(function() {
     console.log("Signed Out");
   }).catch(function(error) {
     console.log(error);
+    base.ref('users').child(currentUser.uid).update({
+      online: true
+    }).then(function(error) {
+      if (error) {
+        console.error(error);
+      } else {
+        // setTimeout(function(){ getOnlineUsers(); }, 1000);
+        getOnlineUsers();
+        console.log("User now offline");
+      }
+    });
   });
 }
 
+function editProfile() {
+  showElement('profile');
+  hideElement('messaging');
+  showElement('btnBackToMessage');
+  hideElement('btnEditProfile');
+  firebase.database().ref('/users/' + currentUser.uid).once('value').then(
+    function(snapshot) {
+      document.getElementById('firstName').value = snapshot.val().firstName;
+      document.getElementById('lastName').value = snapshot.val().lastName;
+      document.getElementById('userName').value = snapshot.val().userName;
+    }
+  );
+}
+
 function saveProfile() {
-
   var user = auth.currentUser;
-
-  var userObj = {
+  var userID = user.uid;
+  base.ref('users').child(userID).update({
     firstName: document.querySelector('#firstName').value,
     lastName: document.querySelector('#lastName').value,
     userName: document.querySelector('#userName').value
-  };
-  var userID = user.uid;
-
-  base.ref('users').child(userID).set(userObj).then(function(error) {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log("Profile Updated");
-      alert("Profile updated", '', 'Success');
+  }).then(
+    function(error) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("Profile Updated");
+        alert("Profile updated", '', 'Success');
+        showElement('messaging');
+        hideElement('profile');
+      }
     }
+  );
+}
+
+function showElement(toShow) {
+  document.getElementById(toShow).classList.add('show');
+  document.getElementById(toShow).classList.remove('hide');
+}
+
+function hideElement(toHide) {
+  document.getElementById(toHide).classList.add('hide');
+  document.getElementById(toHide).classList.remove('show');
+}
+
+function backToMessage() {
+  showElement('btnEditProfile');
+  hideElement('btnBackToMessage');
+  showElement('messaging');
+  hideElement('profile');
+}
+
+function getOnlineUsers() {
+  var onlineUsersDiv = document.getElementById("onlineUsersDiv");
+  onlineUsersDiv.innerHTML = '';
+  var users = base.ref("users");
+  users.on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var user = childSnapshot.val();
+      if (user.online) {
+        var u = document.createElement("b");
+        u.textContent = user.userName;
+        var uElement = document.createElement("div");
+        uElement.appendChild(u);
+        document.getElementById("onlineUsersDiv").appendChild(uElement);
+      }
+    });
   });
+}
+
+function sendMessage() {
+  var input = document.querySelector('#msgInput');
+  if (auth.currentUser) {
+    if (input != '') {
+      var now = new Date();
+      var messageObj = {
+        message: input.value,
+        timestamp: now.toString(),
+        user: auth.currentUser.uid,
+      };
+      var convoList = document.getElementsByClassName('convo');
+      var convo;
+      console.log(convoList);
+      for (var i = 0; i < convoList.length; i++) {
+        if (convoList[i].style.display == 'block') {
+          convo = convoList[i].id.slice(7);
+          console.log(convo);
+        }
+      }
+base.ref('conversations/' + convo + '/messages/').push(messageObj, function() {
+        console.log("Message Sent!");
+      });
+      //Clear input box
+      input.value = '';
+    } else {
+      alert("Please enter a message to send");
+    }
+  } else {
+    alert("Please log in to send messages");
+  }
+
+}
+
+function getMessages(convo) {
+  if (!document.getElementById('msgFeed' + convo)) {
+    var newConvo = document.createElement('div');
+    newConvo.id = "msgFeed" + convo;
+    newConvo.classList.add('convo');
+  } else {
+    var newConvo = document.getElementById("msgFeed" + convo)
+  }
+  var messages = base.ref("conversations/" + convo + '/messages/');
+  msgCount = 0;
+  messages.on('child_added', function(snapshot) {
+    document.getElementById('beep').play();
+
+    var msg = snapshot.val();
+    var test = msg.message;
+    var msgUsernameElement = document.createElement("b");
+    msgUsernameElement.style.cursor = "pointer";
+    firebase.database().ref('/users/' + msg.user).once('value').then(function(snapshot) {
+      msgUsernameElement.textContent = "User: " + snapshot.val().userName;
+      msgUsernameElement.addEventListener('click', function() {
+        privateMessage(msg.user);
+      });
+    });
+
+    var msgTextElement = document.createElement("p");
+    msgTextElement.textContent = msg.message;
+    var msgElement = document.createElement("div");
+    var msgDelete = document.createElement("div");
+
+    msgElement.id = "msg" + snapshot.key;
+    msgDelete.textContent = "DELETE";
+    msgDelete.style.color = "red";
+    msgDelete.style.cursor = "pointer";
+    msgElement.classList.add('message');
+    msgDelete.addEventListener('click', function() {
+      deleteMessage(snapshot.key);
+    });
+    msgElement.appendChild(msgUsernameElement);
+    msgElement.appendChild(msgTextElement);
+    if (auth.currentUser) {
+      firebase.database().ref('/users/' + currentUser.uid).once('value').then(function(snapshot) {
+        if (snapshot.val().type == 'admin') {
+          msgElement.appendChild(msgDelete);
+          console.log("Admin logged in");
+        }
+      });
+    }
+    if (auth.currentUser) {
+      if (msg.user == currentUser.uid) {
+        msgElement.classList.add('ownMessage');
+      }
+    }
+    newConvo.appendChild(msgElement);
+    document.querySelector('#msgContainer').appendChild(newConvo);
+    scrollToBottom('msgContainer');
+    openTab(convo);
+  });
+}
+
+function getConvos() {
+  convos = [];
+  base.ref('/users/' + currentUser.uid + '/conversations/').once('value').then(function(snapshot) {
+    var i = 0;
+    snapshot.forEach(function(child){
+        var key = child.key;
+        convos[i] = key;
+        var x = child.val().id;
+        var to;
+        base.ref('conversations/' + x + '/users/').once('value').then(function(snapshot) {
+            var users = snapshot.val();
+            for (var i = 0; i < users.length; i++) {
+              if (users[i] != auth.currentUser.uid)
+              console.log(users[i]);
+              to = users[i];
+            }
+            base.ref('/users/' + to).once('value').then(function(snapshot2){
+              to = snapshot2.val().userName;
+            });
+        });
+        //Build DOM
+        //<button class="w3-bar-item w3-button" onclick="openTab('Main')">Main</button>
+        
+        //creating the tab box
+        var tabBox = document.createElement('div');
+        tabBox.classList.add('tabBox');
+        tabBox.innerHTML = to;        
+        tabBox.id = "tab" + key;
+        
+        //creating the tab to go in the tab box
+        var tab = document.createElement('div');
+        
+        
+        tab.classList.add('tab');      
+        tab.addEventListener('click', function(){ openTab(key);});
+        document.getElementById('tabs').appendChild(tab);
+        
+        //append the tab to the tabBox
+        tab.appendChild(tabBox);
+        
+        
+
+        i++;
+    });
+  });
+}
+
+function deleteMessage(msgID) {
+  if (confirm('Are you sure you want to delete this message?')) {
+    base.ref('conversations/' + convo + '/messages/').child(msgID).remove();
+    console.log("Message deleted");
+    var msgElement = document.getElementById('msg' + msgID);
+    msgElement.parentNode.removeChild(msgElement);
+  }
+}
+
+function privateMessage(to) {
+  var key;
+  if (auth.currentUser) {
+    var now = new Date();
+    var convoObj = {
+      users: [auth.currentUser.uid, to],
+      timestamp: now.toString(),
+    };
+    base.ref('conversations/').push(convoObj, function() {}).then(function(snapshot) {
+      base.ref('users/' + currentUser.uid + '/conversations/' + snapshot.key).set({
+        id: snapshot.key
+      });
+      base.ref('users/' + to + '/conversations/' + snapshot.key).set({
+        id: snapshot.key
+      });
+      document.querySelector('#msgInput').value = '';
+      var newConvo = document.createElement('div');
+      newConvo.id = "msgFeed" + snapshot.key;
+      newConvo.classList.add('convo');
+      document.querySelector('#msgContainer').appendChild(newConvo);
+      getConvos();
+      getMessages(snapshot.key);
+    });
+  } else {
+    alert("Please log in to private message");
+  }
+}
+
+
+
+
+
+function openTab(convo) {
+  var i;
+  var x = document.getElementsByClassName("convo");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  if (!document.getElementById('msgFeed' + convo)) {
+    getMessages(convo);
+      switchTab(convo);
+  }
+  document.getElementById('msgFeed' + convo).style.display = "block";
+  scrollToBottom('msgContainer');
+}
+
+
+
+
+
+//function to switch tabs
+function switchTab(convo) {
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].classList.remove = "active";
+  }
+  document.getElementById('tab' + convo).classList.add = 'active';
+}
+
+
+
+
+
+// emoji
+function addEmoji() {
+  var x = document.getElementById("pickEmoji");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function insertSmiley(smiley){
+
+  var currentText = document.getElementById("msgInput");
+
+  var smileyWithPadding = " " + smiley + " ";
+  currentText.value += smileyWithPadding;
+  currentText.focus();
+}
+
+function PlaySound(soundObj) {
+  var sound = document.getElementById(soundObj);
+  sound.play();
+}
+
+function scrollToBottom(id) {
+  var div = document.getElementById(id);
+  div.scrollTop = div.scrollHeight - div.clientHeight;
+}
+function enable(element) {
+  document.querySelector(element).disabled = false;
+}
+function disable(element) {
+  document.querySelector(element).disabled = true;
 }
